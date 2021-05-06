f36d44f255acc201fd70c0b793fd977669bbe79c
Roshan Pratap Katel <paradise98425@gmail.com>
2021-05-06 16:20:26 +0545

    addPosts function added

--- route.go
+++ route.go
@@ -25,9 +25,32 @@ func getPosts(resp http.ResponseWriter, req *http.Request) {
 	result, err := json.Marshal(posts)
 	if err != nil {
 		resp.WriteHeader(http.StatusInternalServerError)
-		resp.Write([]byte(`{"message": "error marshalling the posts array"}`))
+		resp.Write([]byte(`{"error": "error marshalling the posts array"}`))
 		return
 	}
 	resp.WriteHeader(http.StatusOK)
 	resp.Write(result)
 }
+
+// function to add the new post 
+func addPosts(resp http.ResponseWriter, req *http.Request) {
+	var post Post 	// variable post is of type Post struct 
+	err := json.NewDecoder(req.Body).Decode(&posts)
+	if err != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error" : "error unmarshalling the request"}`))
+		return
+	}
+	// creating the post id
+	post.Id = len(posts) + 1
+	// adding the post to the array of posts
+	posts = append(posts, post)
+	result, err2 := json.Marshal(post)
+	if err2 != nil {
+		resp.WriteHeader(http.StatusInternalServerError)
+		resp.Write([]byte(`{"error" : "error marshalling the response"}`))
+		return
+	}
+	resp.WriteHeader(http.StatusOK)
+	resp.Write(result)
+}	
